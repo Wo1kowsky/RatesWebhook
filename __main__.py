a429ad1c42d3eb4dcea4c50a7d0a496f6ff21500
maximvolkov <maxix.5858@gmail.com>
2020-03-18 14:35:58 +0300

    added commands

--- __main__.py
+++ __main__.py
@@ -5,4 +5,4 @@ import json
 def get_rate(params):
     type_of_currency = params.get("currency")
     today_date = datetime.date.today()
-    return ({'rate' : str(pycbrf.ExchangeRates(today_date)[type_of_currency].rate)})
+    return {'rate' : str(pycbrf.ExchangeRates(today_date)[type_of_currency].rate)}
